060fe0a5cdff3119bb67af8b34de28a90ba769a8
Rajshah7 <63506258+Rajshah7@users.noreply.github.com>
2026-05-26 13:01:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
-//your JS code here. If required.
+const output = document.getElementById('output');
+
+output.innerHTML = `
+	<tr id="loading">
+		<td colspan="2">Loading</td>
+	</tr>
+`;
+
+const start = performance.now();
+
+function createPromise(name){
+	const delay = Math.random() * 2 + 1;
+
+	const promise = new Promise((resolve) =>{
+		setTimeout(() => {
+			resolve({
+				name,
+			time: delay 
+			})
+		},delay * 1000)
+	})
+}
+
+const promise1 = createPromise("Promise 1");
+const promise2 = createPromise("Promise 2");
+const promise3 = createPromise("Promise 3");
+
+Promise.all([promise1,promise2,promise3]).then((results) => {
+	
+	output.innerHTML = "";
+
+	results.forEach((result) => {
+		const row = document.createElement("tr");
+
+		row.innerHTML = `
+			<td>${result.name}</td>
+			<td>${result.time.toFixed(3)}</td>
+		`
+
+		output.append(row);
+	})
+
+	const totalTime =
+      (performance.now() - startTime) / 1000;
+
+    // Add total row
+    const totalRow = document.createElement("tr");
+
+    totalRow.innerHTML = `
+      <td>Total</td>
+      <td>${totalTime.toFixed(3)}</td>
+    `;
+
+    output.appendChild(totalRow);
+})
